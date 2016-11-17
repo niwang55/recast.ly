c6a95e5eb965210a5afe7145981ff1d2884692cc
niwang55 <nickwang55@gmail.com>
2016-11-16 20:55:33 -0800

    Complete bare minimum requirements up to live search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,60 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.options = {
+      key: window.YOUTUBE_API_KEY,
+      query: 'Pokemon Sun Moon',
+      max: 10
+    };
+
+    // this.videoData = [];
+
+    this.callback = (data) => { 
+      // console.log('App.js', this.videoData, data);
+      // for (var i = 0; i < data.length; i++) {
+      //   this.videoData.push(data[i]);
+      // }
+      this.setState({
+        allVids: data,
+        currentVid: data[0]
+      });
+      // console.log('App.js2', this.videoData, data);
+    };
+    
+    this.state = {
+      allVids: emptyVideoData,
+      currentVid: emptyVideoData[0]
+    };
+
+  }
+
+  onVideoListEntryClick(video) {
+    this.setState({
+      currentVid: video
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVid} state={this.state}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.allVids} state={this.state} onVideoListEntryClick={this.onVideoListEntryClick.bind(this)}/>
+        </div>
+      </div>
+      );
+  }
+
+  componentDidMount() {
+    console.log('mount');
+    this.props.searchYouTube(this.options, this.callback.bind(this));
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,25 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    dataType: 'json',
+    data: {
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+      type: 'video',
+      videoEmbeddable: 'true',
+      part: 'snippet'
+    },
+    success: function (data) {
+      console.log('Youtube: success', data.items);
+      callback(data.items);
+    },
+    error: function (data) {
+      console.error('Youtube: error', data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
